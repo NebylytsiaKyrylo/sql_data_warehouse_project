de03d688490a78d44a0f4a0a03152bca06f4e4d6
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2026-03-31 18:24:13 +0200

    Add script to load CRM sales details into Silver Layer

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -2,27 +2,25 @@
 TRUNCATE TABLE silver.crm_cust_info;
 
 -- Inserting Data Into: silver.crm_cust_info
-INSERT INTO
-    silver.crm_cust_info (
-                             cst_id,
-                             cst_key,
-                             cst_firstname,
-                             cst_lastname,
-                             cst_marital_status,
-                             cst_gndr,
-                             cst_create_date
-                         )
-WITH
-    crm_cust_info_dedup AS (
-                               SELECT
-                                   *,
-                                   ROW_NUMBER()
-                                   OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC, dwh_load_date DESC) AS r_num
-                               FROM bronze.crm_cust_info
-                               WHERE
-                                     cst_id IS NOT NULL
-                                 AND TRIM(cst_id) ~ '^[0-9]+$'
-    )
+INSERT INTO silver.crm_cust_info (
+    cst_id,
+    cst_key,
+    cst_firstname,
+    cst_lastname,
+    cst_marital_status,
+    cst_gndr,
+    cst_create_date
+)
+WITH crm_cust_info_dedup AS (
+    SELECT
+        *,
+        ROW_NUMBER()
+        OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC, dwh_load_date DESC) AS r_num
+    FROM bronze.crm_cust_info
+    WHERE
+          cst_id IS NOT NULL
+      AND TRIM(cst_id) ~ '^[0-9]+$'
+                            )
 
 SELECT
     CAST(TRIM(cst_id) AS INT) AS cst_id,
@@ -57,17 +55,16 @@ WHERE
 TRUNCATE TABLE silver.crm_prd_info;
 
 -- Inserting Data Into: silver.crm_prd_info
-INSERT INTO
-    silver.crm_prd_info(
-                           prd_id,
-                           prd_cat_id,
-                           prd_key,
-                           prd_nm,
-                           prd_cost,
-                           prd_line,
-                           prd_start_dt,
-                           prd_end_dt
-                       )
+INSERT INTO silver.crm_prd_info(
+    prd_id,
+    prd_cat_id,
+    prd_key,
+    prd_nm,
+    prd_cost,
+    prd_line,
+    prd_start_dt,
+    prd_end_dt
+)
 SELECT
     CAST(TRIM(prd_id) AS INT) AS prd_id,
     REPLACE(LEFT(prd_key, 5), '-', '_') AS prd_cat_id,
@@ -95,5 +92,82 @@ WHERE
       prd_id IS NOT NULL
   AND prd_id ~ '^[0-9]+$';
 
+-- Truncating Table: silver.crm_prd_info
+TRUNCATE TABLE silver.crm_sales_details;
 
+-- Inserting Data Into: silver.crm_sales_details
+INSERT INTO silver.crm_sales_details(
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales,
+    sls_quantity,
+    sls_price
+)
+WITH crm_sales_values AS (
+    SELECT
+        TRIM(sls_ord_num) AS sls_ord_num,
+        TRIM(sls_prd_key) AS sls_prd_key,
+        CAST(TRIM(sls_cust_id) AS INT) AS sls_cust_id,
+
+        CASE
+            WHEN TRIM(sls_order_dt) ~ '^\d{8}$' THEN TO_DATE(sls_order_dt, 'YYYYMMDD')
+            ELSE NULL
+        END AS sls_order_dt,
+
+        CASE
+            WHEN TRIM(sls_ship_dt) ~ '^\d{8}$' THEN TO_DATE(sls_ship_dt, 'YYYYMMDD')
+            ELSE NULL
+        END AS sls_ship_dt,
+
+        CASE
+            WHEN TRIM(sls_due_dt) ~ '^\d{8}$' THEN TO_DATE(sls_due_dt, 'YYYYMMDD')
+            ELSE NULL
+        END AS sls_due_dt,
+
+        ABS(CAST(COALESCE(NULLIF(TRIM(sls_sales), ''), '0') AS NUMERIC)) AS sls_sales,
+        ABS(CAST(COALESCE(NULLIF(TRIM(sls_quantity), ''), '0') AS NUMERIC)) AS sls_quantity,
+        ABS(CAST(COALESCE(NULLIF(TRIM(sls_price), ''), '0') AS NUMERIC)) AS sls_price
+
+    FROM bronze.crm_sales_details
+                         )
+SELECT
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+
+    CASE
+        WHEN sls_order_dt IS NULL THEN sls_ship_dt - INTERVAL '1 day'
+        ELSE sls_order_dt
+    END AS sls_order_dt,
+
+    CASE
+        WHEN sls_ship_dt IS NULL THEN sls_order_dt + INTERVAL '1 day'
+        ELSE sls_ship_dt
+    END AS sls_ship_dt,
+
+    CASE
+        WHEN sls_due_dt IS NULL THEN sls_ship_dt + INTERVAL '5 days'
+        ELSE sls_due_dt
+    END AS sls_due_dt,
+
+    CASE
+        WHEN (sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * sls_price)
+            AND (sls_quantity > 0 AND sls_price > 0)
+            THEN sls_quantity * sls_price
+        ELSE sls_sales
+    END AS sls_sales,
+
+    sls_quantity,
+
+    CASE
+        WHEN (sls_price IS NULL OR sls_price <= 0 OR sls_price != sls_sales / NULLIF(sls_quantity, 0))
+            AND (sls_sales > 0 AND sls_quantity > 0)
+            THEN sls_sales / sls_quantity
+        ELSE sls_price
+    END AS sls_price
+FROM crm_sales_values;
 
